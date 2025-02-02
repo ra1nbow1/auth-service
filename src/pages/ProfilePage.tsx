93d7716f799f530938102dade6cb08cd77693e02
Matvey Romanov <youtuberm@yandex.ru>
2025-02-02 13:11:32 +0300

    Improved routing
    Signed-off-by: Matvey Romanov <youtuberm@yandex.ru>

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -40,6 +40,7 @@ const ProfilePage: React.FC = () => {
     }
 
     if (!data) {
+        navigate('/login');
         return (
             <div className="flex items-center justify-center min-h-screen">
                 <div className="p-4 w-full max-w-md text-center">
